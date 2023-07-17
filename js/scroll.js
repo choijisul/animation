25d294c36ccdce4cbb2c215cedb8ffe002df5d67
user.email.w2217@e-mirim.hs.kr <user.email.w2217@e-mirim.hs.kr>
2023-07-17 23:01:06 +0900

    character - scroll

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+
+const container = document.querySelector('.scroll-container');
+const scrollSpeed = 2.11; // 스크롤 속도를 조절하는 값
+
+container.addEventListener('wheel', (event) => {
+    event.preventDefault(); // 기본 스크롤 동작 방지
+
+    const scrollAmount = event.deltaY * scrollSpeed; // 마우스 휠의 이동량
+
+    // 이미지를 움직이기 위해 스크롤할 양 계산
+    const currentScroll = container.scrollLeft;
+    container.scrollTo({
+        left: currentScroll + scrollAmount,
+        behavior: 'smooth' // 부드러운 스크롤을 원하면 'smooth'로 설정하세요.
+    });
+});
---
+++ js/slideImg.js
@@ -0,0 +1,19 @@
+// const slides = document.querySelector(".slides");
+// const leftbtn = document.querySelector('.toleft');
+// const rightbtn = document.querySelector('.toright');
+// let slideNo = 0;
+// rightbtn.addEventListener("click", function goSlide() {
+//     if (slideNo < 4) {
+//         slideNo += 1;
+//         document.querySelector(`#slide${slideNo}`).style.transform = `translateX(${-102.7 * slideNo}%)`;
+//     }
+// }
+// );
+// leftbtn.addEventListener("click", function goSlide() {
+//     if (slideNo > 0) {
+//         slideNo -= 1;
+//         const whatwillchange = document.querySelector(`#slide${slideNo + 1}`).style;
+//         whatwillchange.transform = `translateX(0%)`;
+//     }
+// }
+// );
